d64b88bab86be5c26ba1187ec936a39f1695b948
samshiya <samshiyabasheer@gmail.com>
2026-06-09 19:49:30 +0530

    implement wipe functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,56 +1,77 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import { resumes } from "../../constants";
 import {usePuterStore} from "~/lib/puter";
-import {useLocation, useNavigate} from "react-router";
-import {useEffect} from "react";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        { title: "Ai resume analyzer" },
-        {
-            name: "description",
-            content:
-                "AI Resume Analyzer is a web application that analyzes resumes using artificial intelligence to provide insights, skill evaluation, and improvement suggestions for better job opportunities",
-        },
+        { title: "Resumind" },
+        { name: "description", content: "Smart feedback for your dream job!" },
     ];
 }
 
 export default function Home() {
-    const {  auth } = usePuterStore();
-
-    const location = useLocation();
-    const next: string = location.search.split("next=")[1];
-
+    const { auth, kv } = usePuterStore();
     const navigate = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
 
     useEffect(() => {
-        if (!auth.isAuthenticated) {
-            navigate(next || "/auth?next=/");
-        }
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
     }, [auth.isAuthenticated])
-    return (
-        <main className="bg-[url('/images/bg-main.svg')] bg-cover ">
-            <Navbar />
 
-            <section className="main-section">
-                <div className="page-heading">
-                    <h1>Track Your Application & Resume Ratings</h1>
+    useEffect(() => {
+        const loadResumes = async () => {
+            setLoadingResumes(true);
 
-                    <h2>
-                        Review Your Submissions and check AI-powered feedback.
-                    </h2>
-                </div>
+            const resumes = (await kv.list('resume:*', true)) as KVItem[];
 
-                {resumes.length > 0 && (
-                    <div className="resumes-section">
-                        {resumes.map((resume) => (
-                            <ResumeCard key={resume.id} resume={resume} />
-                        ))}
-                    </div>
+            const parsedResumes = resumes?.map((resume) => (
+                JSON.parse(resume.value) as Resume
+            ))
+
+            setResumes(parsedResumes || []);
+            setLoadingResumes(false);
+        }
+
+        loadResumes()
+    }, []);
+
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
+
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Track Your Applications & Resume Ratings</h1>
+                {!loadingResumes && resumes?.length === 0 ? (
+                    <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                ): (
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
                 )}
-            </section>
-        </main>
-    );
+            </div>
+            {loadingResumes && (
+                <div className="flex flex-col items-center justify-center">
+                    <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+                </div>
+            )}
+
+            {!loadingResumes && resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume} />
+                    ))}
+                </div>
+            )}
+
+            {!loadingResumes && resumes?.length === 0 && (
+                <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                    <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                        Upload Resume
+                    </Link>
+                </div>
+            )}
+        </section>
+    </main>
 }
